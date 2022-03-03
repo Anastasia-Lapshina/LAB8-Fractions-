d82ac1b51d96fa6644fa072f0500a6b5b1f046bd
Anastasia Lapshina <anapluslap@gmail.com>
2022-03-03 21:23:07 +0300

    Deleted the new part for Sting expressions

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,4 @@
 package com.company;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Fraction {
 
@@ -131,55 +129,5 @@ public class Fraction {
         String result;
         result = numerator + "/" + denominator;
         return result;
-    }
-
-    public static boolean ExpressionCheck(String expression) {
-        return expression.matches("^\\d+/\\d+[-+:*]\\d+/\\d+|^-\\d+/\\d+[-+:*]\\d+/\\d+");
-    }
-
-    public static String ExtractingTheSymbol(String expression) throws Exception {
-        String Symbol;
-        Pattern pattern = Pattern.compile("\\d[-+:*]\\d");
-        Matcher matcher = pattern.matcher(expression);
-
-        if (matcher.find()) {  //necessary for the piece of code to perform
-            Symbol = matcher.group().substring(1, 2);
-            return Symbol;
-        }
-        else {
-            System.out.println("Invalid data. Please, try and enter an expression again.");
-            throw new Exception();
-        }
-    }
-
-    public static int IndexOfTheSymbol(String expression) throws Exception {
-        String Symbol;
-        int indexOfSymbol;
-        int count = 0;
-
-        if(expression.charAt(0) == '-'){  //if the first fraction starts with "-", remove the first "minus" symbol
-            expression = expression.substring(1);
-            count += 1;
-        }
-
-        Pattern pattern = Pattern.compile("[0-9][-+:*]\\d");
-        Matcher matcher = pattern.matcher(expression);
-
-
-        if (matcher.find()) {  //necessary for the piece of code to perform correctly
-            Symbol = matcher.group(0).substring(1, 2);
-            indexOfSymbol = expression.indexOf(Symbol);
-            if (count == 1) {
-                return indexOfSymbol + 1;
-            }
-            else {
-                return indexOfSymbol;
-            }
-        }
-
-        else {
-            System.out.println("Invalid data. Please, enter your expression again.");
-            throw new Exception();
-        }
     }
 }
